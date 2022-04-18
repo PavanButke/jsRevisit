e5886d151aeeb245ae2ec69a17fb4f851d1c9986
PavanButke <butkepavan55@gmail.com>
2022-04-18 18:18:12 +0530

    step3 asyncAwait

--- asyncAwait.js
+++ asyncAwait.js
@@ -5,7 +5,7 @@
 //step1 async lagana ; toh async toh function pe lagate hai
 //toh yeh hint h ki sare fetch wale code ko function me daalo
 
-function fetchKrBhai(){
+async function fetchKrBhai(){
 
 //step 2 paste the 1st then() wala code and with fetch reqst offcourse
 
@@ -18,7 +18,7 @@ let respone = await  fetch('https://api.coinpaprika.com/v1/coins/btc-bitcoin', {
         //here we are accepting data from api call and storing it in respone
         //function ko krdiya saaf 
         console.log('Entered Then')
-        const data =  response.json();  // now we are just formatting data into json format
+        const data = await response.json();  // now we are just formatting data into json format
         
         console.log(data)
      
