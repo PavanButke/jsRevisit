ab9caefd5a6c0505d18c68220a7275c226d128ca
PavanButke <butkepavan55@gmail.com>
2022-04-19 14:53:02 +0530

    silentFailure 2

--- silentFailure.js
+++ silentFailure.js
@@ -1,12 +1,15 @@
 // in this example we are going to see use of strict mode
 //1. in this keyword
 //2. in getters and setters
+'use strict'
 
- console.log(this) // here this is at global scope ; so this=window
+ console.log("hi", this) // here this is at global scope ; so this=window
 
  function functionalScope(){
-     console.log(this) //now this is inside functionalScope  this != window
+     console.log("hello", this) //now this is inside functionalScope  this != window
                         // but now still you'll see in console of your brwsr that this=window
                         //which is wrong ; so how to avoid this 
                         //using strict mode 
- }
+                    }
+
+functionalScope()
