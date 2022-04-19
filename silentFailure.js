08e498d83f1674f772a8aa8d66cd4288ec4060f6
PavanButke <butkepavan55@gmail.com>
2022-04-19 15:05:18 +0530

    silentFailure 3

--- silentFailure.js
+++ silentFailure.js
@@ -1,15 +1,35 @@
 // in this example we are going to see use of strict mode
 //1. in this keyword
 //2. in getters and setters
-'use strict'
+// 'use strict'
 
- console.log("hi", this) // here this is at global scope ; so this=window
+//  console.log("hi", this) // here this is at global scope ; so this=window
 
- function functionalScope(){
-     console.log("hello", this) //now this is inside functionalScope  this != window
-                        // but now still you'll see in console of your brwsr that this=window
-                        //which is wrong ; so how to avoid this 
-                        //using strict mode 
-                    }
+//  function functionalScope(){
+//      console.log("hello", this) //now this is inside functionalScope  this != window
+//                         // but now still you'll see in console of your brwsr that this=window
+//                         //which is wrong ; so how to avoid this 
+//                         //using strict mode 
+//                     }
 
-functionalScope()
+// functionalScope()
+
+//silent failure in getters and setters
+
+let keyValPair ={
+    a:200,
+    b:0,
+
+    //getter function likhte - getter mtlb read only function , just a fancy name
+
+    get koisafun(){
+        return this.b
+    },
+
+    set koisaSet(_value){
+        this.b = _value //  ideally it should give error but as you can see there is no error
+    }                   // to avoid this we use strict mode
+}
+
+
+keyValPair.koisafun = -100
