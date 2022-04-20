1ac47d2ef430c4114db7e29436ce47c3a9f3d1ae
PavanButke <butkepavan55@gmail.com>
2022-04-20 16:46:16 +0530

    basic PCM 2

--- ProtoTypeClassMethods.js
+++ ProtoTypeClassMethods.js
@@ -1,5 +1,11 @@
-//step1 :- let's start with creating a class
+//step3 : create a new class now try to access object from , this newly created class
+class dharm {
+        politics(){
+            console.log('Hindu Muslim Dangey');
+        }
+}
 
+//step1 :- let's start with creating a class
 class PM{
     constructor(kaam){
         this.work = kaam;
@@ -10,3 +16,4 @@ class PM{
 
 const moodi = new PM("Corruption");
 console.log(moodi)
+moodi.dharm(); // will this work
