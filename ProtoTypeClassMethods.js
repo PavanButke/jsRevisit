274604c484d5ae9d63a828ef46ec6ee9d80917b4
PavanButke <butkepavan55@gmail.com>
2022-04-20 16:50:46 +0530

    basic PCM 3

--- ProtoTypeClassMethods.js
+++ ProtoTypeClassMethods.js
@@ -1,13 +1,14 @@
 //step3 : create a new class now try to access object from , this newly created class
 class dharm {
         politics(){
-            console.log('Hindu Muslim Dangey');
+            console.log('Jaat Paat Dangae fassad');
         }
 }
 
 //step1 :- let's start with creating a class
 class PM{
     constructor(kaam){
+        s
         this.work = kaam;
     }
     //step2: create a construction ; which will create objects
@@ -15,5 +16,7 @@ class PM{
 }
 
 const moodi = new PM("Corruption");
-console.log(moodi)
-moodi.dharm(); // will this work
+// console.log(moodi)
+moodi.dharm(); // will this work ..no it will not work we are getting Uncaught
+                // instead of that it should have printed 'Hindu Muslim Dangey'
+
